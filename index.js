6e57d3628d919f8c9e4c2d4044035fa2f4e5f62b
Marc <marc.ferraro1@gmail.com>
2021-02-03 10:35:35 -0600

    commit tree 305aea4d2e2feda9b6aa53c5ab9622d2d8631635

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  const hour = timeString.split(":")[0];
+  if (parseInt(hour) < 12){
+    return "Good Morning"
+  } else if (parseInt(hour) > 12 && parseInt(hour) < 17){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  const greeting = document.getElementById('greeting')
+
+  greeting.innerText = string
+
+}
